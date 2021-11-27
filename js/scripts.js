a5abfceb37a9a21437eaeb879bea8fc8b7c446ea
devzhxpr <79888029+devzhxpr@users.noreply.github.com>
2021-11-28 03:11:07 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,3 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
-
-
-const wait = (delay = 0) =>
-  new Promise(resolve => setTimeout(resolve, delay));
-
-const setVisible = (elementOrSelector, visible) => 
-  (typeof elementOrSelector === 'string'
-    ? document.querySelector(elementOrSelector)
-    : elementOrSelector
-  ).style.display = visible ? 'block' : 'none';
-
-setVisible('.body', false);
-setVisible('.loadingbox', true);
-
-document.addEventListener('DOMContentLoaded', () =>
-  wait(1000).then(() => {
-    setVisible('body', true);
-    setVisible('.loadingbox', false);
-  }));
